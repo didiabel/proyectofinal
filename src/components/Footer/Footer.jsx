dbb267817ead6b09dcf9a8a540861974e953ae07
DidiAbel <didiabel123@gmail.com>
2022-01-27 16:32:50 -0300

    ultimo commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,7 @@ const Footer = () => {
   return (
     <div className="w-100">
     <div className="redes p-3 ">
-    <a href="https://www.facebook.com/profile.php?id=100077536331922"rel="nofollow" target="_blank"><i className="bi bi-facebook"></i></a>
-    <a href="https://www.instagram.com/tecno_green12/?hl=es-la" rel="nofollow" target="_blank"><i className="bi bi-instagram"></i></a>
-    <a href="https://www.youtube.com/watch?v=GUBkhoaK-tc"rel="nofollow" target="_blank"><i className="bi bi-youtube"></i></a>
+    
     <form className=" m-3" action="https://submit-form.com/bA76s9dw">
       <div className="d-flex justify-content-around">
         <div className="carta p-3">
@@ -38,6 +36,9 @@ const Footer = () => {
   </div>
   </div>
 </form>
+<a href="https://www.facebook.com/profile.php?id=100077536331922"rel="nofollow" target="_blank"><i className="bi bi-facebook"></i></a>
+    <a href="https://www.instagram.com/tecno_green12/?hl=es-la" rel="nofollow" target="_blank"><i className="bi bi-instagram"></i></a>
+    <a href="https://www.youtube.com/watch?v=GUBkhoaK-tc"rel="nofollow" target="_blank"><i className="bi bi-youtube"></i></a>
     <h6 className="text-center p-4 footer"> © Sitio hecho por TecnoGreen 2022 </h6>
    </div>
    </div>
